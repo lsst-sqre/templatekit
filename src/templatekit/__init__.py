ce96220449e69c27d58b1f97fdd049824059340f
Russ Allbery <rra@lsst.org>
2022-03-14 12:02:05 -0700

    Use importlib.metadata for module version
    Obtain the module version using importlib.metadata instead of
    pkg_resources, and export a version_info variable as well matching
    how Safir works.  This gets rid of mypy warnings for using
    setuptools modules without type stubs.

--- src/templatekit/__init__.py
+++ src/templatekit/__init__.py
@@ -1,11 +1,20 @@
-from pkg_resources import DistributionNotFound, get_distribution
+__all__ = ["TemplatekitExtension", "__version__", "version_info"]
 
-try:
-    __version__ = get_distribution("templatekit").version
-except DistributionNotFound:
-    # package is not installed
-    pass
+from importlib.metadata import PackageNotFoundError, version
 
 from .jinjaext import TemplatekitExtension
 
-__all__ = ["TemplatekitExtension"]
+__version__: str
+"""The version string of Templatekit (PEP 440 / SemVer compatible)."""
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "0.0.0"
+
+version_info = __version__.split(".")
+"""The decomposed version, split across "``.``."
+
+Use this for version comparison.
+"""
